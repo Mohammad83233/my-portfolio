ff601897f27b0555bac1e7d0eb6a08660baa9448
Mohammad83233 <24mp2262@rit.ac.in>
2026-06-25 13:49:48 +0400

    Update LinkedIn URL format in data.js

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -13,7 +13,7 @@ export const PROFILE_DATA = {
   availability: 'Immediate',
 
   // Social Links
-  linkedin: 'https://linkedin.com/in/mohammad-nisham',
+  linkedin: 'www.linkedin.com/in/mohammad-nisham-b151022b4',
   github: 'https://github.com/mohammad83233',
 
   certifications: [
